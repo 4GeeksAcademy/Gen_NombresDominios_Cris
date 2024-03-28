a4432f8a4cea49bacbb5aa87011c66c823f3f942
cris4crisland <160337227+cris4crisland@users.noreply.github.com>
2024-03-28 16:28:11 +0000

    primera prueba

---
+++ app.js
@@ -0,0 +1,15 @@
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let domains = [];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; i++) {
+    for (let k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k]);
+      domains.push(pronoun[i] + adj[j] + noun[k]);
+      console.log(domains);
+    }
+  }
+}
